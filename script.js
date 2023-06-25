87593ed687e26f21658aa8bcdcd9788a856fc8ad
Michael Edgington <57604493+medgington00@users.noreply.github.com>
2023-06-25 02:22:48 -0400

    v1.2.0
    -Added tutorial

--- script.js
+++ script.js
@@ -34,12 +34,16 @@ const resultText = document.getElementById("result-text");
 
 const container = document.getElementById("container");
 
-let winnablecount = 0;
-let testingcount = 10;
-let isTesting = false;
-let currenttest = 0;
-
 let isTwoTie = false;
+
+let isInTutorial = false;
+let tutorialSlide = -1;
+const tutorialContainer = document.getElementById("tutorial-container");
+const tutorialText = tutorialContainer.getElementsByClassName("tutorial-text")[0];
+const thOne = document.getElementById("th-1");
+const thTwo = document.getElementById("th-2");
+const thThree = document.getElementById("th-3");
+const thFour = document.getElementById("th-4");
 
 localStorage['streakSTORE'] = '0';
 localStorage['tableSTORE'];
@@ -47,7 +51,7 @@ localStorage['cardColorSTORE'];
 localStorage['gamesPlayedSTORE'];
 localStorage['winnableGamesSTORE'];
 
-function OnLoad() {
+function OnLoad() {    
     streak = parseInt(localStorage['streakSTORE'] || 'defaultValue');
 
     if (localStorage.getItem("gamesPlayedSTORE") === null) {
@@ -62,11 +66,7 @@ function OnLoad() {
     SetTableBackground(localStorage.getItem("tableSTORE"));
     SetCardColor(localStorage.getItem("cardColorSTORE"));
 
-    if(isTesting) {
-        TestingLoop(currenttest);
-    } else {
-        NewGame();
-    }
+    NewGameNoTutorial();
 }
 
 function NewGame() {
@@ -120,62 +120,76 @@ function NewGame() {
 
     extraCards = new Array();
 
-    //Setup Heart Cards
-    while(heartNumbers.length > 3) {
-        let tempCard = heartCard.cloneNode(true);
-        heartRow.appendChild(tempCard);
+    if(!isInTutorial) {
+        //Setup Heart Cards
+        while(heartNumbers.length > 3) {
+            let tempCard = heartCard.cloneNode(true);
+            heartRow.appendChild(tempCard);
 
-        const rand = Math.floor(Math.random() * heartNumbers.length);
-        const num = heartNumbers[rand];
-        heartNumbers = RemoveArrayItem(num,heartNumbers);
+            const rand = Math.floor(Math.random() * heartNumbers.length);
+            const num = heartNumbers[rand];
+            heartNumbers = RemoveArrayItem(num,heartNumbers);
 
-        SetCardNumber(tempCard, num);
+            SetCardNumber(tempCard, num);
 
-        heartCards.push(tempCard);
-    }
-    //Heart Extras
-    while(heartNumbers.length > 0) {
-        let tempCard = heartCard.cloneNode(true);
-        heartExtrasRow.appendChild(tempCard);
+            heartCards.push(tempCard);
+        }
+        //Heart Extras
+        while(heartNumbers.length > 0) {
+            let tempCard = heartCard.cloneNode(true);
+            heartExtrasRow.appendChild(tempCard);
 
-        const rand = Math.floor(Math.random() * heartNumbers.length);
-        const num = heartNumbers[rand];
-        heartNumbers = RemoveArrayItem(num,heartNumbers);
+            const rand = Math.floor(Math.random() * heartNumbers.length);
+            const num = heartNumbers[rand];
+            heartNumbers = RemoveArrayItem(num,heartNumbers);
 
-        SetCardNumber(tempCard, num);
+            SetCardNumber(tempCard, num);
 
-        extraCards.push(tempCard);
-    }
-    //Setup Diamond Cards
-    while(diamondNumbers.length > 3) {
-        let tempCard = diamondCard.cloneNode(true);
-        diamondRow.appendChild(tempCard);
+            extraCards.push(tempCard);
+        }
+        //Setup Diamond Cards
+        while(diamondNumbers.length > 3) {
+            let tempCard = diamondCard.cloneNode(true);
+            diamondRow.appendChild(tempCard);
 
-        const rand = Math.floor(Math.random() * diamondNumbers.length);
-        const num = diamondNumbers[rand];
-        diamondNumbers = RemoveArrayItem(num,diamondNumbers);
+            const rand = Math.floor(Math.random() * diamondNumbers.length);
+            const num = diamondNumbers[rand];
+            diamondNumbers = RemoveArrayItem(num,diamondNumbers);
 
-        SetCardNumber(tempCard, num);
+            SetCardNumber(tempCard, num);
 
-        diamondCards.push(tempCard);
-    }
-    //Diamond Extras
-    while(diamondNumbers.length > 0) {
-        let tempCard = diamondCard.cloneNode(true);
-        diamondExtrasRow.appendChild(tempCard);
+            diamondCards.push(tempCard);
+        }
+        //Diamond Extras
+        while(diamondNumbers.length > 0) {
+            let tempCard = diamondCard.cloneNode(true);
+            diamondExtrasRow.appendChild(tempCard);
 
-        const rand = Math.floor(Math.random() * diamondNumbers.length);
-        const num = diamondNumbers[rand];
-        diamondNumbers = RemoveArrayItem(num,diamondNumbers);
+            const rand = Math.floor(Math.random() * diamondNumbers.length);
+            const num = diamondNumbers[rand];
+            diamondNumbers = RemoveArrayItem(num,diamondNumbers);
 
-        SetCardNumber(tempCard, num);
+            SetCardNumber(tempCard, num);
 
-        extraCards.push(tempCard);
-    }
-    //Setup Spade Cards
-    while(spadeNumbers.length > 3) {
+            extraCards.push(tempCard);
+        }
+        //Setup Spade Cards
+        while(spadeNumbers.length > 3) {
+            let tempCard = spadeCard.cloneNode(true);
+            spadeRow.appendChild(tempCard);
+
+            const rand = Math.floor(Math.random() * spadeNumbers.length);
+            const num = spadeNumbers[rand];
+            spadeNumbers = RemoveArrayItem(num,spadeNumbers);
+
+            SetCardNumber(tempCard, num);
+
+            spadeCards.push(tempCard);
+        }
+        //Spade Extras
+        while(spadeNumbers.length > 0) {
         let tempCard = spadeCard.cloneNode(true);
-        spadeRow.appendChild(tempCard);
+        spadeExtrasRow.appendChild(tempCard);
 
         const rand = Math.floor(Math.random() * spadeNumbers.length);
         const num = spadeNumbers[rand];
@@ -183,28 +197,24 @@ function NewGame() {
 
         SetCardNumber(tempCard, num);
 
-        spadeCards.push(tempCard);
+        extraCards.push(tempCard);
+        }
+
+    } else {
+        SetTutorialCards();
     }
-    //Spade Extras
-    while(spadeNumbers.length > 0) {
-    let tempCard = spadeCard.cloneNode(true);
-    spadeExtrasRow.appendChild(tempCard);
-
-    const rand = Math.floor(Math.random() * spadeNumbers.length);
-    const num = spadeNumbers[rand];
-    spadeNumbers = RemoveArrayItem(num,spadeNumbers);
-
-    SetCardNumber(tempCard, num);
-
-    extraCards.push(tempCard);
-
-    if(isTesting) {
-        GameOver();
+    
+    if(!isInTutorial) {
+        NextColumn();
     }
+    
 }
 
-
-    NextColumn();
+function NewGameNoTutorial() {
+    HideTutorialElements();
+    tutorialContainer.style.display = "none";
+    isInTutorial = false;
+    NewGame();
 }
 
 function FlipToBack(card) {
@@ -230,7 +240,8 @@ function SelectHeart() {
     if(playerNum > oppNum) {
         points++;
     } else if(playerNum == 2 && oppNum == 2) {
-        isTwoTie = true;
+        if(!isInTutorial)
+            isTwoTie = true;
     } else {
     }
 
@@ -261,7 +272,8 @@ function SelectDiamond() {
     if(playerNum > oppNum) {
         points++;
     } else if(playerNum == 2 && oppNum == 2) {
-        isTwoTie = true;
+        if(!isInTutorial)
+            isTwoTie = true;
     } else {
     }
 
@@ -308,6 +320,32 @@ function NextColumn() {
           }, 500);  
     }
 }   
+
+function PreviousColumn() {
+    if(currentColumn>=0) {
+        heartCards[currentColumn].onclick = "";
+        diamondCards[currentColumn].onclick = "";
+        heartCards[currentColumn].classList.remove("active-card");
+        diamondCards[currentColumn].classList.remove("active-card");
+    }
+    
+    if(currentColumn>0) {
+
+        currentColumn-=1;
+
+        diamondCards[currentColumn].classList.add("active-card"); 
+
+        heartCards[currentColumn].classList.add("active-card"); 
+
+        FlipToBack(spadeCards[currentColumn+1]);   
+        FlipToBack(diamondCards[currentColumn]);
+        FlipToBack(heartCards[currentColumn]);
+
+        heartCards[currentColumn].onclick = function() {SelectHeart()};
+    
+        diamondCards[currentColumn].onclick = function() {SelectDiamond()};
+    }
+}
 
 function SetPointsText() {
     if(points < 5)
@@ -521,11 +559,319 @@ function SetTableBackground(num) {
         root.style.setProperty("--table-bg",rootStyle.getPropertyValue("--table-bg-three"));
 }
 
-function TestingLoop(i) {
-    console.log("enter testing loop");
-    if(i < testingcount) {
-        NewGame();
-    } else {
-        console.log("Winnable Games Percentage: " + winnablecount / testingcount);
+function StartTutorial() {
+    isInTutorial = true;
+
+    NewGame();
+
+    heartCards[0].classList.remove("active-card");
+    diamondCards[0].classList.remove("active-card");
+
+    tutorialContainer.style.display = "flex";
+    
+    tutorialSlide = -1;
+    SetTutorialSlide(1);
+}
+
+//Advances or rewinds tutorial based on -1 = rewind and 1 = advance
+function SetTutorialSlide(i) {
+    if(i == -1 && tutorialSlide > 0) {
+        HideTutorialElements()
+        tutorialSlide-=1;
     }
+    if(i == 1 && tutorialSlide < 50) {
+        HideTutorialElements()
+        tutorialSlide+=1;
+    }
+
+    if(tutorialSlide == 0) {
+
+        tutorialText.innerHTML = "Welcome to Altior!<br>Press the right arrow to advance.";
+        FlipToBack(spadeCards[0]);
+
+    } else if(tutorialSlide==1) {
+
+        tutorialText.innerHTML = "The player sees their card first.";
+        FlipToFront(spadeCards[0]);
+        thOne.style.opacity="1";
+
+    } else if(tutorialSlide==2) {
+
+        tutorialText.innerHTML = "They must then pick between the opponents two suits<br>Let's go with hearts.";
+        FlipToBack(heartCards[0]);
+        thTwo.style.opacity="1";
+        winText.innerHTML = "Points: 0"
+
+    } else if(tutorialSlide==3) {
+
+        tutorialText.innerHTML = "You won this round since 10 is higher than 5.";
+        FlipToFront(heartCards[0]);
+        FlipToBack(spadeCards[1]);
+        winText.innerHTML = "Points: 1"
+        
+    } else if(tutorialSlide==4) {
+
+        tutorialText.innerHTML = "Let's go for another.";
+        FlipToFront(spadeCards[1]);
+        FlipToBack(heartCards[1]);
+        
+    } else if(tutorialSlide==5) {
+
+        tutorialText.innerHTML = "You lost this round since Ace is higher than Queen.";
+        FlipToFront(heartCards[1]);
+        FlipToBack(spadeCards[2]);
+
+    } else if(tutorialSlide==6) {
+
+        tutorialText.innerHTML = "Tough luck. Let's try again.";
+        FlipToFront(spadeCards[2]);
+        FlipToBack(heartCards[2]);
+
+        
+    } else if(tutorialSlide==7) {
+
+        tutorialText.innerHTML = "Another loss round since ties go to the opponent.";
+        FlipToFront(heartCards[2]);
+        FlipToBack(spadeCards[3]);
+
+        
+    } else if(tutorialSlide==8) {
+
+        tutorialText.innerHTML = "This isn't going too well, we are due for a lucky break.";
+        FlipToFront(spadeCards[3]);
+        FlipToBack(heartCards[3]);
+        winText.innerHTML = "Points: 1"
+        winText.style.color = "white";
+
+    } else if(tutorialSlide==9) {
+
+        tutorialText.innerHTML = "Look at that! A Two-Tie is when both players have a 2. This results in an instant game win for the player.";
+        FlipToFront(heartCards[3]);
+        FlipToBack(spadeCards[4]);
+
+        winText.innerHTML = "Points: 6"
+        winText.style.color = "green";
+
+    } else if(tutorialSlide==10) {
+
+        tutorialText.innerHTML = "Let's try out diamonds now.";
+        FlipToFront(spadeCards[4]);
+        FlipToBack(diamondCards[4]);
+        FlipToBack(diamondCards[3]);
+        FlipToBack(diamondCards[2]);
+        FlipToBack(diamondCards[1]);
+        FlipToBack(diamondCards[0]);
+
+    } else if(tutorialSlide==11) {
+
+        tutorialText.innerHTML = "When you select a card, all cards of that suit to the left will also be flipped";
+        FlipToFront(diamondCards[4]);
+        FlipToFront(diamondCards[3]);
+        FlipToFront(diamondCards[2]);
+        FlipToFront(diamondCards[1]);
+        FlipToFront(diamondCards[0]);
+        FlipToBack(extraCards[0]);
+        FlipToBack(extraCards[1]);
+        FlipToBack(extraCards[2]);
+        thThree.style.opacity = "1";
+        
+    } else if(tutorialSlide==12) {
+
+        tutorialText.innerHTML = "Each game there will be 3 cards of each suit that are not used.";
+        FlipToFront(extraCards[0]);
+        FlipToFront(extraCards[1]);
+        FlipToFront(extraCards[2]);
+        thFour.style.opacity = "1";
+    } else if(tutorialSlide==13) {
+
+        tutorialText.innerHTML = "In order to win the player must score at least 6 points or a Two-Tie";
+        winText.classList.add("th-5");
+        tutorialContainer.getElementsByClassName("tutorial-button")[1].innerHTML = "&#8594;";
+
+    } else if(tutorialSlide==14) {
+
+        tutorialText.innerHTML = "Streaks are tracked here.<br> Good luck!";
+        streakText.classList.add("th-5");
+        tutorialContainer.getElementsByClassName("tutorial-button")[1].innerHTML = "Exit";
+
+    } else if(tutorialSlide==15) {
+
+        tutorialContainer.style.display = "none";
+        NewGameNoTutorial();
+    }
+    
+    
+    
+}
+
+function HideTutorialElements() {
+    thOne.style.opacity="0";
+    thTwo.style.opacity="0";
+    thThree.style.opacity="0";
+    thFour.style.opacity="0";
+    winText.classList.remove("th-5");
+    streakText.classList.remove("th-5");
+}
+
+function SetTutorialCards() {
+    let tempCard;
+    //Set Heart Cards
+    tempCard = heartCard.cloneNode(true);
+    heartRow.appendChild(tempCard);
+    SetCardNumber(tempCard,"5");
+    heartCards.push(tempCard);
+
+    tempCard = heartCard.cloneNode(true);
+    heartRow.appendChild(tempCard);
+    SetCardNumber(tempCard,"A");
+    heartCards.push(tempCard);
+
+    tempCard = heartCard.cloneNode(true);
+    heartRow.appendChild(tempCard);
+    SetCardNumber(tempCard,"9");
+    heartCards.push(tempCard);
+
+    tempCard = heartCard.cloneNode(true);
+    heartRow.appendChild(tempCard);
+    SetCardNumber(tempCard,"2");
+    heartCards.push(tempCard);
+
+    tempCard = heartCard.cloneNode(true);
+    heartRow.appendChild(tempCard);
+    heartCards.push(tempCard);
+    tempCard = heartCard.cloneNode(true);
+    heartRow.appendChild(tempCard);
+    heartCards.push(tempCard);
+    tempCard = heartCard.cloneNode(true);
+    heartRow.appendChild(tempCard);
+    heartCards.push(tempCard);
+    tempCard = heartCard.cloneNode(true);
+    heartRow.appendChild(tempCard);
+    heartCards.push(tempCard);
+    tempCard = heartCard.cloneNode(true);
+    heartRow.appendChild(tempCard);
+    heartCards.push(tempCard);
+    tempCard = heartCard.cloneNode(true);
+    heartRow.appendChild(tempCard);
+    heartCards.push(tempCard);
+
+    //Heart Extras
+    tempCard = heartCard.cloneNode(true);
+    heartExtrasRow.appendChild(tempCard);
+    SetCardNumber(tempCard,"K");
+    extraCards.push(tempCard);
+
+    tempCard = heartCard.cloneNode(true);
+    heartExtrasRow.appendChild(tempCard);
+    SetCardNumber(tempCard,"10");
+    extraCards.push(tempCard);
+
+    tempCard = heartCard.cloneNode(true);
+    heartExtrasRow.appendChild(tempCard);
+    SetCardNumber(tempCard,"4");
+    extraCards.push(tempCard);
+
+    //Setup Diamond Cards
+    tempCard = diamondCard.cloneNode(true);
+    diamondRow.appendChild(tempCard);
+    SetCardNumber(tempCard,"7");
+    diamondCards.push(tempCard);
+
+    tempCard = diamondCard.cloneNode(true);
+    diamondRow.appendChild(tempCard);
+    SetCardNumber(tempCard,"5");
+    diamondCards.push(tempCard);
+
+    tempCard = diamondCard.cloneNode(true);
+    diamondRow.appendChild(tempCard);
+    SetCardNumber(tempCard,"2");
+    diamondCards.push(tempCard);
+
+    tempCard = diamondCard.cloneNode(true);
+    diamondRow.appendChild(tempCard);
+    SetCardNumber(tempCard,"Q");
+    diamondCards.push(tempCard);
+
+    tempCard = diamondCard.cloneNode(true);
+    diamondRow.appendChild(tempCard);
+    SetCardNumber(tempCard,"9");
+    diamondCards.push(tempCard);
+
+    tempCard = diamondCard.cloneNode(true);
+    diamondRow.appendChild(tempCard);
+    diamondCards.push(tempCard);
+    tempCard = diamondCard.cloneNode(true);
+    diamondRow.appendChild(tempCard);
+    diamondCards.push(tempCard);
+    tempCard = diamondCard.cloneNode(true);
+    diamondRow.appendChild(tempCard);
+    diamondCards.push(tempCard);
+    tempCard = diamondCard.cloneNode(true);
+    diamondRow.appendChild(tempCard);
+    diamondCards.push(tempCard);
+    tempCard = diamondCard.cloneNode(true);
+    diamondRow.appendChild(tempCard);
+    diamondCards.push(tempCard);
+
+    tempCard = diamondCard.cloneNode(true);
+    diamondExtrasRow.appendChild(tempCard);
+    extraCards.push(tempCard);
+    tempCard = diamondCard.cloneNode(true);
+    diamondExtrasRow.appendChild(tempCard);
+    extraCards.push(tempCard);
+    tempCard = diamondCard.cloneNode(true);
+    diamondExtrasRow.appendChild(tempCard);
+    extraCards.push(tempCard);
+
+    //Setup Spade Cards
+    tempCard = spadeCard.cloneNode(true);
+    spadeRow.appendChild(tempCard);
+    SetCardNumber(tempCard,"10");
+    spadeCards.push(tempCard);
+
+    tempCard = spadeCard.cloneNode(true);
+    spadeRow.appendChild(tempCard);
+    SetCardNumber(tempCard,"Q");
+    spadeCards.push(tempCard);
+
+    tempCard = spadeCard.cloneNode(true);
+    spadeRow.appendChild(tempCard);
+    SetCardNumber(tempCard,"9");
+    spadeCards.push(tempCard);
+
+    tempCard = spadeCard.cloneNode(true);
+    spadeRow.appendChild(tempCard);
+    SetCardNumber(tempCard,"2");
+    spadeCards.push(tempCard);
+
+    tempCard = spadeCard.cloneNode(true);
+    spadeRow.appendChild(tempCard);
+    SetCardNumber(tempCard,"4");
+    spadeCards.push(tempCard);
+
+    tempCard = spadeCard.cloneNode(true);
+    spadeRow.appendChild(tempCard);
+    spadeCards.push(tempCard);
+    tempCard = spadeCard.cloneNode(true);
+    spadeRow.appendChild(tempCard);
+    spadeCards.push(tempCard);
+    tempCard = spadeCard.cloneNode(true);
+    spadeRow.appendChild(tempCard);
+    spadeCards.push(tempCard);
+    tempCard = spadeCard.cloneNode(true);
+    spadeRow.appendChild(tempCard);
+    spadeCards.push(tempCard);
+    tempCard = spadeCard.cloneNode(true);
+    spadeRow.appendChild(tempCard);
+    spadeCards.push(tempCard);
+    tempCard = spadeCard.cloneNode(true);
+
+    spadeExtrasRow.appendChild(tempCard);
+    extraCards.push(tempCard);
+    tempCard = spadeCard.cloneNode(true);
+    spadeExtrasRow.appendChild(tempCard);
+    extraCards.push(tempCard);
+    tempCard = spadeCard.cloneNode(true);
+    spadeExtrasRow.appendChild(tempCard);
+    extraCards.push(tempCard);
 }
